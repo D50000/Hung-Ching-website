a0dacaee5ce3aa58e1d7502598549af06fe97359
D5000 <aoe102198@gmail.com>
2017-12-25 23:37:35 +0800

    upload Hung-Ching-website
    Initial the Hung-Ching-website

---
+++ en/home.php
@@ -0,0 +1,62 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
+"http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=big5">
+<title>HungChing Limited Com</title>
+<style type="text/css">
+<!--
+html {
+background-image: url(image/blackstreet200.png);
+background-position: center center;
+}
+
+body {
+background-image: url(image/BG<?
+$numb = rand(1,2);
+echo rand(1,2) ;
+
+
+
+
+
+
+?>.jpg);
+background-position: center 0;
+background-repeat:no-repeat;
+}
+.style1 {color: #CCCCCC}
+
+-->
+</style></head>
+
+<body>
+<div align="center">
+  <table width="1000" border="0" cellspacing="0" cellpadding="0">
+    <tr>
+      <td><object classid="clsid:D27CDB6E-AE6D-11cf-96B8-444553540000" codebase="http://download.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=6,0,29,0" width="1000" height="100">
+        <param name="movie" value="flash/Menu7.swf">
+        <param name=quality value=high>
+        <embed src="flash/Menu7.swf" quality=high pluginspage="http://www.macromedia.com/shockwave/download/index.cgi?P1_Prod_Version=ShockwaveFlash" type="application/x-shockwave-flash" width="1000" height="100"></embed>
+      </object></td>
+    </tr>
+    <tr>
+      <td height="500">&nbsp;</td>
+    </tr>
+    <tr>
+      <td><div align="center">
+        <object classid="clsid:D27CDB6E-AE6D-11cf-96B8-444553540000" codebase="http://download.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=6,0,29,0" width="800" height="120">
+          <param name="movie" value="flash/Menu8.swf">
+          <param name=quality value=high>
+          <embed src="flash/Menu8.swf" quality=high pluginspage="http://www.macromedia.com/shockwave/download/index.cgi?P1_Prod_Version=ShockwaveFlash" type="application/x-shockwave-flash" width="800" height="120"></embed>
+        </object>
+      </div></td>
+    </tr>
+    <tr>
+      <td height="0"><p>&nbsp;</p>      </td>
+    </tr>
+  </table>
+  <p class="style1">c2012 Hung-Ching Company . All Rights Reserved.</p>
+</div>
+</body>
+</html>
